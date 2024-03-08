9c0ff8df26496b5d1d23f8e1b40758ee1b2928a2
Toufiq Zaman <toufiqmkzaman99@gmail.com>
2024-03-08 20:29:50 +0800

    new file

---
+++ FarenheitToCelcius.java
@@ -0,0 +1,13 @@
+
+    import Java.util.Scanner:
+    
+    public class FahrenheitToCelsius {
+    public static void main(String [] args) }
+
+    system.out.prinln("please input farenheit:");
+
+    Scanner input =new Scanner(System.in);
+    double fahrenheit = input.nextouble ():
+    double celsius - (9.0 / 5) * (farenheit - 32);
+    System.out.println("celsaus:"+celsius);    
+}
