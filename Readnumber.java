031a72eae3ab5bfc50022f710df345fba96cf8a8
Toufiq Zaman <toufiqmkzaman99@gmail.com>
2024-03-08 20:12:40 +0800

    first git file

---
+++ Readnumber.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Readnumber {
+
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+System.out.println("please input radius:");
+double radius = input.nextDouble();
+
+System.out.println("age");
+ int age = input.nextInt();
+
+System.out.println("distance:");
+long distance = input.nextLong();
+
+System.out.println("radius:" + radius +",age:"+ age +",distance: "+ distance);
+
+  }
+}
+
