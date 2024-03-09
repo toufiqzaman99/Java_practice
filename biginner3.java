8edb0d8a5d5d5a5390b29198843fb99cfab4477a
Toufiq Zaman <toufiqmkzaman99@gmail.com>
2024-03-10 01:54:07 +0800

    name&number

---
+++ biginner3.java
@@ -0,0 +1,11 @@
+public class biginner3 {
+   public static void main(String[] args) {
+    
+    System.out.println("NAME : TOUFIQ ZAMAN");
+    System.out.print("PHONE NUMBER: ");
+    System.out.print("01310000000");
+
+
+   } 
+}
+ 
