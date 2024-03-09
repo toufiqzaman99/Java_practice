5af38f9a82f4c19dc874edf91a9d8cb84155ac43
Toufiq Zaman <toufiqmkzaman99@gmail.com>
2024-03-10 01:19:47 +0800

    bangladesh

---
+++ beginnern2.java
@@ -0,0 +1,7 @@
+public class beginnern2 {
+    public static void main(String[] args) {
+        System.out.println("hello bangladesh");
+        System.out.println("love u bangladesh");
+    }
+
+}
