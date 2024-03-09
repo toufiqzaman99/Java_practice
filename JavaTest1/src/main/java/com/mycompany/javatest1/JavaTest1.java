aebfef97510a56f298cefa3cf850e2b02aab261e
Toufiq Zaman <toufiqmkzaman99@gmail.com>
2024-03-10 01:14:43 +0800

    name practice

---
+++ JavaTest1/src/main/java/com/mycompany/javatest1/JavaTest1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javatest1;
+
+/**
+ *
+ * @author Hp ElitBook
+ */
+public class JavaTest1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
