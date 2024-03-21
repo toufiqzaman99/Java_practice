661fc1ddc45be29145d8625a6a25f61874086d78
Toufiq Zaman <toufiqmkzaman99@gmail.com>
2024-03-21 09:29:53 +0800

    string

---
+++ StringMethod.java
@@ -0,0 +1,10 @@
+
+    public class StringMethod {
+        public static void main(String[] args) {
+        String message = " Welcome to java class";
+         System.out.println("The length of" + message + " is" + message.length());
+        }
+        }
+        
+  
+
