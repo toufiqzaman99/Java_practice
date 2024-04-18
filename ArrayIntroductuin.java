1438599635e7e7a241558ffd5d4df316ce0ad252
Toufiq Zaman <toufiqmkzaman99@gmail.com>
2024-04-18 09:07:35 +0800

    array

---
+++ ArrayIntroductuin.java
@@ -0,0 +1,7 @@
+public class ArrayIntroductuin {
+    public static void main(String[] args) {
+        double [] myArray =new double[10];
+
+        System.out.println("The length of myArray is:"+ myArray.length);
+    }
+}
